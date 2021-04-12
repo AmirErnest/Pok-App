ebd395572305059f6057fca344dfd773be610379
Amir Ernest <amir.m.ernest@gmail.com>
2021-04-12 21:46:18 +0200

    creating array of pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-
-alert('Hello world');
-
-var favoriteFood = "Ice Cream";
-document.write(favoriteFood);
+//array to hold pokemon list
+let pokemonList = [{name: "Charmeleon", height: 1.1, types: ["fire"]},
+                   {name: "Raichu", height: 0.8, type: ["electric"]},
+                   {name: "Rhyhorn", height: 1, type: ["rock", "ground"]}];
+                   
+console.log(pokemonList);
