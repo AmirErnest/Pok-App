2aef053b11ee058321bab8f203cbc0e11c2fdc23
Amir Ernest <amir.m.ernest@gmail.com>
2021-04-25 14:55:31 +0200

    IIEF function with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,44 @@
-//array to hold pokemon list
-let pokemonList = [
-                    {
-                      name: "Charmeleon",
-                      height: 1.1,
-                      type: ["fire"]
-                    },
-                    {
-                      name: "Raichu",
-                      height: 0.8,
-                      type: ["electric"]
-                    },
-                    {
-                      name: "Rhyhorn",
-                      height: 1,
-                      type: ["rock", "ground"]
-                    }
-                 ];
+//array to hold pokemonRepository list within IIEF function
+//whatever will be returned from it will be the content of the pokemon repository
+// only what is returned  at the end of the IIEF, is what can be accessed when pokemonRepo is mentioned.
+
+var pokemonRepository = (function() {
+  let pokemonList = [
+      {
+        name: "Charmeleon",
+        height: 1.1,
+        type: ["fire"]
+      },
+      {
+        name: "Raichu",
+        height: 0.8,
+        type: ["electric"]
+      },
+      {
+        name: "Rhyhorn",
+        height: 1,
+        type: ["rock", "ground"]
+      }
+    ];
+
+    function getAll() {
+      return pokemonList;
+    }
+
+    function add(item) {
+      pokemonList.push(item);
+    }
+
+// the return object has reference to the local functions in the IIEF
+    return {
+      getAll: getAll,
+      add: add
+    };
+}) ();
 
 //looping through the array list of pokemons using foreach(), printing out the name and height.
-pokemonList.forEach(function(pokemon){
-    document.write("<p> Name: </p>" + pokemon.name + " " +
-                  " <p> height: </p>" + pokemon.height + " " +
-                  " <p> Type: </p>" + pokemon.type + "<br>");
+pokemonRepository.getAll().forEach((item) => {
+  document.write( "<p> Name: </p>" + item.name + " " +
+                  "<p> height: </p>" + item.height + " " +
+                  "<p> Type: </p>" + item.type + "<br>");
 });
